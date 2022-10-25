aa09af79796c11bfbd4071cc573c9bc4dba4b409
Dritan Harizaj <dharizaj@gmail.com>
2022-10-25 22:04:41 +0200

    added toast

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import CacheBuster from './CacheBuster';
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
 
-class App extends Component {
-  render() {
-    return (
-      <CacheBuster>
-        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-          if (loading) return null;
-          if (!loading && !isLatestVersion) {
-            alert('new version! must refresh')
-            // refreshCacheAndReload();
-          }
+const notify = () => toast('Here is your toast.');
 
-          return (
-            <div className="App">
-              <header className="App-header">
-                <h1>Cache Busting - Example</h1>
-                <p>
-                  Bundle version - <code>v{global.appVersion}</code>
-                </p>
-              </header>
-            </div>
-          );
-        }}
-      </CacheBuster>
-    );
-  }
-}
+const App = () => {
+  return (
+    <CacheBuster>
+      {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+        if (loading) return null;
+        if (!loading && !isLatestVersion) {
+          alert('Une nouvelle version de Viavoo est publié. Veuillez actualiser la page.')
+          // refreshCacheAndReload();
+        }
+
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h1>Cache Busting - Example</h1>
+              <p>
+                Bundle version - <code>v{global.appVersion}</code>
+              </p>
+              <div>
+                <button onClick={notify}>Make me a toast</button>
+                <Toaster />
+              </div>
+            </header>
+          </div>
+        );
+      }}
+    </CacheBuster>
+  );
+};
 
 export default App;
